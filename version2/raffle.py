25c79edbb14bc0ed2b53ae9255aecca54626bd7c
helloarielyoung <helloariel@hotmail.com>
2017-04-13 04:49:18 +0000

    fixed scripts so they run correctly

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,27 @@
 """Randomly pick customer and print customer info"""
 
 # Add code starting here
-
+from customers import get_customers_from_file
+import random
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print "Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    print pick_winner(customers)
+
+run_raffle()
